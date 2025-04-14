1aa68e52a1e3c5e372f847348cf55e005581767f
Yosuke Fukuda <yf@lambda.ski>
2025-04-14 11:20:34 +0900

    Revise

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 npx rescript build
-for x in rescript/*.js; do
-  if [[ "$(basename "$x")" != "concave-hull.js" ]]; then
-    npx parcel build "$x" --no-scope-hoist
-  fi
-done
+if [ "$#" -eq 1 ]; then
+  npx parcel build $1 --no-scope-hoist
+else
+  for x in rescript/*.js; do
+    if [[ "$(basename "$x")" != "concave-hull.js" ]]; then
+      npx parcel build "$x" --no-scope-hoist
+    fi
+  done
+fi
 cp rescript/concave-hull.js static/js
-# hugo --gc --minify build
-# find public -type f -name "*.html" | while read -r file; do
-#   tidy -quiet -indent --wrap 0 -modify "$file" 2>/dev/null
-#   echo "Prettify: $file"
-# done
